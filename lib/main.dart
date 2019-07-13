001d16c83c60dc48960c62d6a6264477e96a4659
Vinnys Lins <vinnysfl@hotmail.com>
2019-07-13 02:00:54 -0300

    Implementing game using Flame

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,100 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/gestures.dart';
 
-void main() => runApp(MyApp());
+import 'package:flame/game.dart';
+import 'package:flame/flame.dart';
+import 'package:flame/time.dart';
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+void main() async {
+  final screenSize = await Flame.util.initialDimensions();
+  final game = MyGame(screenSize);
+
+  runApp(game.widget);
+
+  Flame.util.addGestureRecognizer(TapGestureRecognizer()
+    ..onTapUp = (TapUpDetails evt) {
+      game.tapUp();
+    }
+    ..onTapDown = (TapDownDetails evt) {
+      game.tapDown();
+    }
+  );
+}
+
+class MyGame extends Game {
+  static const double PLAYER_SPEED = 300;
+  static const double ENEMY_SPEED = 100;
+  static final Paint white = Paint()..color = Color(0xFFFFFFFF);
+  static final Paint black = Paint()..color = Color(0xFF000000);
+
+  Size _screenSize;
+  Rect _player;
+  Rect _background;
+
+  List<Rect> _enemies = [];
+
+  int playerDirection = 1;
+  bool playerMoving = true;
+
+  Random random = Random();
+  Timer _enemyCreator;
+
+  MyGame(this._screenSize) {
+    _background = Rect.fromLTWH(0, 0, _screenSize.width, _screenSize.height);
+    _player = Rect.fromLTWH(_screenSize.width / 2 - 25, 500, 50, 50);
+
+    _enemyCreator = Timer(2, repeat: true, callback: () {
+      _enemies.add(Rect.fromLTWH((random.nextInt(_screenSize.width.toInt()) - 50).toDouble(), 0, 50, 50));
+    });
+
+    _enemyCreator.start();
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+  void tapUp() {
+    playerMoving = true;
+  }
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  void tapDown() {
+    playerMoving = false;
+  }
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
+  void render(Canvas canvas) {
+    canvas.drawRect(_background, white);
+    canvas.drawRect(_player, black);
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+    _enemies.forEach((rect) {
+      canvas.drawRect(rect, black);
     });
   }
 
   @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+  void update(double dt) {
+    if (playerMoving) {
+      _player = _player.translate(PLAYER_SPEED * dt * playerDirection, 0);
+
+      if (_player.right >= _screenSize.width && playerDirection == 1) {
+        playerDirection = -1;
+      }
+
+      if (_player.left <= 0 && playerDirection == -1) {
+        playerDirection = 1;
+      }
+    }
+
+    for (var i = 0; i < _enemies.length; i++) {
+      final Rect rect = _enemies[i];
+      _enemies[i] = rect.translate(0, ENEMY_SPEED * dt);
+
+      if (rect.overlaps(_player)) {
+        print('Morreu');
+      }
+    }
+
+    _enemies.removeWhere((rect) => rect.top >= _screenSize.height || rect.overlaps(_player));
+
+    _enemyCreator.update(dt);
   }
 }
